35bfc9b4a38855264f6c175aadd870b43f76e7c5
Andrew Pivovarov <Andrew Pivovarov>
2020-03-31 19:35:53 +0300

    add UserRepo, User, ProfileService

--- src/main/java/com/pivovarov/nedosteam/NedosteamController.java
+++ src/main/java/com/pivovarov/nedosteam/NedosteamController.java
@@ -1,16 +1,26 @@
 package com.pivovarov.nedosteam;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class NedosteamController {
+    @Autowired
+    private ProfileService profileService;
 
-    @GetMapping("/profile")
-    public String profile(@RequestParam()String login, Model model) {
-        model.addAttribute("login", login);
+    @GetMapping("/profile/{nickname}/{age}/{email}")
+    public String profile(@PathVariable("nickname")String nickname,
+                          @PathVariable("age")String age,
+                          @PathVariable("email")String email,
+                          Model model) {
+        User user = new User(nickname, Integer.parseInt(age), email);
+        User savedUser = profileService.saveUser(user);
+        model.addAttribute("nickname", nickname);
+        model.addAttribute("age", age);
+        model.addAttribute("email", email);
         return "profile";
     }
 
